26e79b6df843a85bd225c9cf0244ced904cc59ad
Ninjaneer324 <kakashiandflute@gmail.com>
2020-05-09 14:58:22 -0400

    read comments

--- bot.py
+++ bot.py
@@ -1,1 +1,8 @@
-print("Just to have something here")
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = commands.Bot(command_prefix=">")
+
+#So I was thinking: the command could be like >mahjong username1 username2 username3 username4
+# to start a game
