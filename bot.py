fc84ffcb7765e27978836a79aeec5466e1ac3cdb
Ninjaneer324 <kakashiandflute@gmail.com>
2020-05-09 15:47:13 -0400

    added start and join commands

--- bot.py
+++ bot.py
@@ -2,7 +2,25 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 
+turns = []
+
+TOKEN = ''
 bot = commands.Bot(command_prefix=">")
 
 #So I was thinking: the command could be like >mahjong username1 username2 username3 username4
 # to start a game
+
+
+
+@bot.command()
+async def start(ctx):
+    await ctx.send("Waiting for players...")
+
+@bot.command()
+async def join(ctx):
+    if(len(turns) >= 4):
+        await ctx.send("4 players already added.")
+    else:
+        await ctx.send(ctx.author.name + " has joined!")
+
+bot.run(TOKEN)
