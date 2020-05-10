a39b51a8800c4c52c86bb9e889dfeb4ce9e14222
m4-z3 <melissajzhang@gmail.com>
2020-05-09 20:04:19 -0500

    changed class variable to object variable and shorten deck reset

--- deck.py
+++ deck.py
@@ -2,79 +2,42 @@ from piece import *
 from random import shuffle
 
 class Deck:
-    type = ""
-    d = []
+    standard_deckSuits = ["Bamboo", "Dot", "Wan"]
+    honor_deckSuits = ["North", "South", "East", "West", "Center", "Fortune", "TV"]
 
-    def __init__(self, t="standard"):
-        self.type = t
+    def __init__(self, type="standard"):
+        self.type = type
+        self.deck = []
         self.reset()
 
     def reset(self):
-        if self.d:
-            self.clear()
-        for i in range(1, 10):
-            t = Piece(i, "Bamboo")
-            self.d.append(t)
-            self.d.append(t)
-            self.d.append(t)
-            self.d.append(t)
-        for i in range(1, 10):
-            t = Piece(i, "Dot")
-            self.d.append(t)
-            self.d.append(t)
-            self.d.append(t)
-            self.d.append(t)
-        for i in range(1, 10):
-            t = Piece(i, "Wan")
-            self.d.append(t)
-            self.d.append(t)
-            self.d.append(t)
-            self.d.append(t)
-        if self.type == "honor":
-            for i in range(4):
-                t = Piece(0, "North")
-                self.d.append(t)
-
-            for i in range(4):
-                t = Piece(0, "South")
-                self.d.append(t)
-
-            for i in range(4):
-                t = Piece(0, "East")
-                self.d.append(t)
-
-            for i in range(4):
-                t = Piece(0, "West")
-                self.d.append(t)
-
-            for i in range(4):
-                t = Piece(0, "Center")
-                self.d.append(t)
-
-            for i in range(4):
-                t = Piece(0, "Fortune")
-                self.d.append(t)
-
-            for i in range(4):
-                t = Piece(0, "TV")
-                self.d.append(t)
+        '''Shuffles the deck'''
+        if self.deck:
+           self.deck.clear()
+        for suit in Deck.standard_deckSuits:
+            for i in range(1, 10):
+                t = Piece(suit, i)
+                for j in range(4):
+                    self.deck.append(t)
+        if self.type.lower == "honor":
+            for suit in Deck.honor_deckSuits:
+                t = Piece(suit)
+                for i in range(4):
+                    self.deck.append(t)
         self.shuffle()
 
-    def clear(self):
-        self.d.clear()
-
     def shuffle(self):
-        shuffle(self.d)
+        shuffle(self.deck)
 
     def size(self):
-        return len(self.d)
+        return len(self.deck)
 
-    def print(self):
-        for p in self.d:
+    def printDeck(self):
+        for p in self.deck:
             print(p.name())
 
-    def draw(self):
-        return self.d.pop(0)
+    def drawFront(self):
+        return self.deck.pop(0)
 
-    def back(self):
-        return self.d.pop()
+    def drawBack(self):
+        return self.deck.pop()
--- piece.py
+++ piece.py
@@ -1,11 +1,7 @@
 class Piece:
-    number = 0
-    suit = ""
-    pos = []
-
-    def __init__(self, n=0, s=""):
-        self.number = n
-        self.suit = s
+    def __init__(self, suit, number=0):
+        self.number = number
+        self.suit = suit
 
     def name(self):
-        return ("" if not self.number else (str(self.number) + " ")) + self.suit
+        return self.suit + ("" if self.number == 0 else (str(self.number) + " "))
