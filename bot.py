cb3ac5625b0444c07b1c56d15b53fae9828bb4b4
m4-z3 <melissajzhang@gmail.com>
2020-05-14 01:19:31 -0500

    session error checking

--- bot.py
+++ bot.py
@@ -5,22 +5,26 @@ from player import Player
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-mahjongSession = Mahjong()
+mahjongSession = None
 TOKEN = ''
 bot = commands.Bot(command_prefix=">")
 
 @bot.command()
 async def start(ctx):
     await ctx.send("Waiting for players...")
+    mahjongSession = Mahjong()
 
 @bot.command()
 async def join(ctx):
-    player = Player(ctx.author.name, str(ctx.author.id))
-    if(not mahjongSession.addPlayer(player)):
-        await ctx.send("4 players already added.")
+    if mahjongSession is not None:
+        player = Player(ctx.author.name, ctx.author.id)
+        if(not mahjongSession.addPlayer(player)):
+            await ctx.send("4 players already added.")
+        else:
+            await ctx.send(ctx.author.name + " has joined!")
+            if mahjongSession.atFullCapacity():
+                mahjongSession.deal()
     else:
-        await ctx.send(ctx.author.name + " has joined!")
-        if mahjongSession.atFullCapacity():
-            mahjongSession.deal()
+        await ctx.send("No mahjong session has started")
 
 bot.run(TOKEN)
